79302098206ec89ecd8fa3afa2b1dcb87d115f3e
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-05-06 20:04:16 -0500

    updating models books.js and index.js

--- models/book.js
+++ models/book.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+// building the database in mongoose to caputre books from my api
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
+  link: String,
+  img: String,
   date: { type: Date, default: Date.now }
 });
 
